016811d75e5e6820a892672640117ac04f2ecd83
Harsimranjit Dhaliwal <harsimranyadu@icloud.com>
2020-04-27 10:14:12 -0400

    Initial Commit

---
+++ 24/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  24
+//
+//  Created by Harsimranjit Dhaliwal on 2020-04-27.
+//  Copyright © 2020 Harsimranjit Dhaliwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
